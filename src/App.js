3a71def4ee11df11e256de6d7f731e1d0d12a8b3
Ayman Qaisari <qaisariayman@gmail.com>
2025-06-05 13:08:24 +0200

    Commit1: Adding this webpage project to my collection in github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3},
+];
+
+
+function MyButton() {
+  const [showLogo, setShowLogo] = useState(false);
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setShowLogo(true);
+    setCount(count + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <button onClick={handleClick}>Click me</button>
+        {showLogo && (
+          <div>
+            <p>Clicked {count} times</p>
+            <img src={logo} className='App-logo' alt='logo'/>
+          </div>
+          
+        )}
+    </div>
+  )
+}
+
+
+
+const styleObject = {
+  width: user.imageSize,
+  height: user.imageSize,
+  borderRadius: '50%'
+};
+
+export default function MyApp() {
+  
+  const listItems = products.map(product => 
+    <li 
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'lightblue' : 'lightgray'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+  return (
+    <div className='App'>
+      <aside>
+        <h1>{user.name}</h1>
+        <img
+          className='avatar'
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          // style={{
+          //   width: user.imageSize,
+          //   height: user.imageSize,
+          //   borderRadius: '50%'
+          // }}
+          style={styleObject}
+        />
+      </aside>
+      <div className='welcome'>
+          <h1>Welcome to my app</h1>
+          {/* <img src={logo} className='App-logo' alt='logo'/> */}
+          <MyButton/>
+          <AboutPage/>
+          <ul>{listItems}</ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br/>How do you do?</p>
+    </>
+  );
+}
+
+// export default MyApp;
